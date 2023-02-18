03b330c7edeb0f0ccb10952db47de7360de03619
Evgene-Kopylov <eugenevkopylov@gmail.com>
2023-02-18 23:26:10 +0200

    движение одиночной линии по экрану

---
+++ app/src/main.rs
@@ -0,0 +1,55 @@
+use macroquad::prelude::*;
+mod settings;
+use settings::{
+    GROUND_COLOR
+};
+
+
+
+struct Line {
+    x: f32,
+    speed: f32,
+}
+
+impl Line {
+    fn new() -> Line {
+        Line {
+            x: screen_width() / 2.,
+            speed: 100.,
+        }
+    }
+
+    fn draw(&self) {
+        draw_line(self.x, 0., self.x, screen_height(), 1., BLUE);
+    }
+
+    fn update(&mut self, dt: f32) {
+        if is_key_down(KeyCode::Left) {
+            self.x -= dt * self.speed;
+        }
+
+        if is_key_down(KeyCode::Right) {
+            self.x += dt * self.speed;
+        }
+    }
+
+
+}
+
+
+
+#[macroquad::main("breakout")]
+async fn main() {
+    let mut line = Line::new();
+
+    loop {
+        clear_background(GROUND_COLOR);
+
+
+        line.update(get_frame_time());
+        line.draw();
+
+
+        next_frame().await
+    }
+}
---
+++ app/src/settings.rs
@@ -0,0 +1,13 @@
+use macroquad::prelude::{
+    YELLOW,
+    ORANGE,
+    Color,
+};
+
+
+pub const GROUND_COLOR: Color = Color::new(0.8, 0.8, 0.8, 1.00);
+pub const UNIT_COLOR: Color = ORANGE;
+pub const SELECTOR_COLOR: Color = YELLOW;
+pub const UNIT_SIZE: (f32, f32) = (60.0, 75.0);
+pub const UNIT_SPEED: f32 = 130.0;
+pub const UNIT_ROTATION_SPEED: f32 = 4.0;
