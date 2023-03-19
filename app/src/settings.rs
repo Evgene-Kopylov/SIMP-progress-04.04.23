bd642b54f469b6dbfff4cd39aa16b49cae313a35
Evgene-Kopylov <eugenevkopylov@gmail.com>
2023-03-19 03:12:09 +0200

    цвет юнита

--- app/src/settings.rs
+++ app/src/settings.rs
@@ -1,11 +1,11 @@
 #![allow(unused)]  // FIXME
-use macroquad::color::GRAY;
+use macroquad::color::{DARKGRAY, GRAY};
 use macroquad::prelude::{YELLOW, ORANGE, Color, Vec2};
 
 
 // pub const GROUND_COLOR: Color = Color::new(0.8, 0.8, 0.8, 1.00);
 pub const GROUND_COLOR: Color = GRAY;
-pub const UNIT_COLOR: Color = ORANGE;
+pub const UNIT_COLOR: Color = DARKGRAY;
 pub const SELECTOR_COLOR: Color = YELLOW;
 // pub const UNIT_SIZE: (f32, f32) = (60.0, 75.0);
 pub const UNIT_SIZE: Vec2 = Vec2::new(40., 40.);
